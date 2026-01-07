30c2534a591cdb46ca04c5df2b7bc037b17062c8
Boody Zuckerberg <abdohassan9090@icloud.com>
2026-01-08 01:48:33 +0200

    Delete Game Admin Dashboard directory

--- Game Admin Dashboard/backend/middlewares/role.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (roles = []) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.admin.role)) {
-      return res.status(403).json({ msg: "Forbidden" });
-    }
-    next();
-  };
-};
